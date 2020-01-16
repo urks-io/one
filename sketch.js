d58aac6cc56a8d74b043c7bf59c29d68ea039c2f
urkskur <urkskurth@gmail.com>
2020-01-16 11:40:58 -0800

    vernsucks

--- sketch.js
+++ sketch.js
@@ -3,6 +3,10 @@ createCanvas(500, 500);
 }
 
 function draw() {
+fill(35, 222, 199);
  ellipse(mouseX, mouseY, 77, 77);
+ fill(47, 188, 77);
  ellipse(300, 300, 40, 40);
+ fill(44, 254, 44);
+ ellipse(400,400, 22, 22);
 }
